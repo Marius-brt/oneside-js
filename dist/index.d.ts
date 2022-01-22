3684605844a43faa51e9763836a10a297bd0ea78
Marius Brouty <marius.brouty@gmail.com>
2022-01-22 13:33:52 +0100

    Remove dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { Application, AppSettings } from './server';
-import { Router } from './router';
-export declare function router(): Router;
-export declare function init(settings?: Partial<AppSettings>): Application;
--- dist/utils.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function print(
-  type: 'info' | 'success' | 'error' | 'failed' | 'log' | 'gray',
-  message: string,
-  fatal?: boolean,
-): void;
-export declare function normalize(path: string): string;
